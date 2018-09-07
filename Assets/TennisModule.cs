1ca90f867ba944b6eb3c12adaf39e88d13f0fd04
Timwi <github@timwi.de>
2018-09-07 08:38:53 +0200

    First commit

---
+++ Assets/TennisModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tennis;
+using UnityEngine;
+using Rnd = UnityEngine.Random;
+
+/// <summary>
+/// On the Subject of Tennis
+/// Created by Timwi
+/// </summary>
+public class TennisModule : MonoBehaviour
+{
+    public KMBombInfo Bomb;
+    public KMBombModule Module;
+    public KMAudio Audio;
+
+    private static int _moduleIdCounter = 1;
+    private int _moduleId;
+
+    void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+    }
+}
